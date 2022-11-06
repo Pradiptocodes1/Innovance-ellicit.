4ec306f52910ad2ec7ba79f39f5375e168b26948
Pradipto Chowdhury <90888280+Pradiptocodes1@users.noreply.github.com>
2022-11-06 14:11:56 +0530

    Uploaded necessary files

---
+++ index.js
@@ -0,0 +1,20 @@
+$(".Card1").click(function(){
+    $(".Card1").addClass("active");
+    setTimeout(function(){$(".Card1").removeClass("active");},100)
+})
+
+$(".A").click(function(){
+    $(".A").addClass("active");
+    setTimeout(function(){$(".A").removeClass("active");},100)
+})
+
+$(".Card3").click(function(){
+    $(".Card3").addClass("active");
+    setTimeout(function(){$(".Card3").removeClass("active");},100)
+})
+
+$(".Card4").click(function(){
+    $(".Card4").addClass("active");
+    setTimeout(function(){$(".Card4").removeClass("active");},100)
+})
+
